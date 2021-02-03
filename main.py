db3bb7f78ec137a30878e97038f499106bfa2881
LuckyBirds <usual.man@gmail.com>
2021-02-03 11:07:18 +0300

    api version

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
-
-#import pymysql
 import pymysql.cursors  
-
 import time
-
 import json
 import re
 from datetime import datetime
 import logging
 
-
-
 app = Flask(__name__)
-
-
 app.config['SECRET_KEY'] = 'you-will-never-guess'
-
 logging.basicConfig(filename='request.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-
-
-
-#import pdb; pdb.set_trace()
-
-
-
-
-   
-
-
-@app.route('/sendmessage', methods=['GET'])
-def sendmessage():
+@app.route('/sendmessage/api/v1.0/', methods=['GET'])
+def sendmessage_v1_0():
     connection = pymysql.connect(
     host =  "127.0.0.1",
     port =  3306,
@@ -55,6 +35,30 @@ def sendmessage():
         connection.close()
     return  "message recieved"
     
+@app.route('/sendmessage/api/v0.0/', methods=['GET'])
+def sendmessage_v0_0():
+    connection = pymysql.connect(
+    host =  "127.0.0.1",
+    port =  3306,
+    user = "socialuser",
+    passwd = "socialpass",
+    charset = "utf8mb4",
+    cursorclass = pymysql.cursors.DictCursor,
+    database = "social")
+
+    message_from = request.args.get('message_from') 
+    message_to = request.args.get('message_to')
+    message_text = request.args.get('message_text')
+    message_date = request.args.get('message_date')
+    requestid = request.args.get('requestid')
+    logging.warning("message date: "+ message_date + " | message_from: " + message_from + " | message_to: " + message_to + " | message_text: " +  message_text + " | requestid: " + requestid )
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('INSERT INTO dialogs (  message_from, message_to ,message_text ,message_date )   VALUES(%s, %s, %s, %s) ', ( message_from, message_to, message_text, message_date),)
+        connection.commit()
+    finally:
+        connection.close()
+    return  "message recieved"
 
 
 
